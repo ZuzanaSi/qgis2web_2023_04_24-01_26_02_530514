db6e2d4a6d6417384e747e8f0249ab4231926868
ZuzanaSi <104940382+ZuzanaSi@users.noreply.github.com>
2023-04-24 01:49:04 +0200

    Initial commit

---
+++ data/ostatnbudovy_4.js
@@ -0,0 +1,19 @@
+var json_ostatnbudovy_4 = {
+"type": "FeatureCollection",
+"name": "ostatnbudovy_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Budova": "Hotel Reich", "y": "49.43780585", "x": "18.78894925", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\hotel reich.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78894925, 49.43780585 ] } },
+{ "type": "Feature", "properties": { "Budova": "Hotel Klappholz", "y": "49.44403562", "x": "18.78768861", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\hotel klappholz.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78768861, 49.44403562 ] } },
+{ "type": "Feature", "properties": { "Budova": "Kostol sv. Bartolomeja", "y": "49.43833781", "x": "18.79150003", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\kostol.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.79150003, 49.43833781 ] } },
+{ "type": "Feature", "properties": { "Budova": "Synagóga", "y": "49.43785817", "x": "18.78654599", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\synagóga.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78654599, 49.43785817 ] } },
+{ "type": "Feature", "properties": { "Budova": "Hotel Politzer", "y": "49.43787212", "x": "18.78761619", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\hotel politzer.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78761619, 49.43787212 ] } },
+{ "type": "Feature", "properties": { "Budova": "Lekáreň U Matky Božej (Salvator)", "y": "49.43855234", "x": "18.78953129", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\lekáreň salvator.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78953129, 49.43855234 ] } },
+{ "type": "Feature", "properties": { "Budova": "rabinát", "y": "49.43789305", "x": "18.78690809", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\rabinát.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78690809, 49.43789305 ] } },
+{ "type": "Feature", "properties": { "Budova": "bitúnok", "y": "49.43876513", "x": "18.79414201", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 18.79414201, 49.43876513 ] } },
+{ "type": "Feature", "properties": { "Budova": "Vojenský barakový tábor", "y": "49.43356384", "x": "18.78363848", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\vojenský barákový tábor.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78363848, 49.43356384 ] } },
+{ "type": "Feature", "properties": { "Budova": "Židovský cintorín", "y": "49.43621339", "x": "18.78171533", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\židovský cintorín.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78171533, 49.43621339 ] } },
+{ "type": "Feature", "properties": { "Budova": "ciduk hadin", "y": "49.43641747", "x": "18.78196746", "Foto": "C:\\Users\\Zuzka\\Desktop\\Dizertačka kniha\\mapka na web 1\\ostatné budovy foto\\ciduk hadin.jpg" }, "geometry": { "type": "Point", "coordinates": [ 18.78196746, 49.43641747 ] } },
+{ "type": "Feature", "properties": { "Budova": "židovská škola", "y": "49.43544418", "x": "18.79355729", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 18.79355729, 49.43544418 ] } }
+]
+}
